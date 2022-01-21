211de5ef227bd25aeaee8683ff4dc67a205b26ad
Mohammad Osman Goni <osmangoni0827@gmail.com>
2022-01-21 18:28:23 +0600

    some update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,8 +6,10 @@ function App() {
     <div>
       <Header />
       <div className='bg-indigo-600 w-full h-screen'></div>
+      <h1>It is ehostel</h1>
     </div>
   )
+  
 }
 
 export default App;
